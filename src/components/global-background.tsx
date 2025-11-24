feeb6a693b6d523b5a7bcc1d7bfd847ce10910f4
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-24 20:38:03 +0000

    not change the collor you can move the background to to front   contineo

--- src/components/global-background.tsx
+++ src/components/global-background.tsx
@@ -5,12 +5,12 @@ import dynamic from 'next/dynamic';
 
 const Threads = dynamic(() => import('@/components/threads-background'), {
   ssr: false,
-  loading: () => <div className="absolute inset-0 w-full h-full bg-background -z-10" />,
+  loading: () => <div className="absolute inset-0 w-full h-full bg-background" />,
 });
 
 export default function GlobalBackground() {
     return (
-        <div className="absolute inset-0 w-full h-full bg-background -z-10">
+        <div className="absolute inset-0 w-full h-full bg-background z-0 opacity-20 pointer-events-none">
             <Threads
               color={[1, 1, 1]}
               amplitude={0.5}
