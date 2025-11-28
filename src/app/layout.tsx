9c6a9b8374e107340a419677be26285761ce4980
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-28 05:05:28 +0000

    i went to veryfy my website to google seacrch console  .code is <meta na

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="f0cPrsulYeH8v31ZulVSmI5rMadO_jUW1zD11wb6XLc" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
